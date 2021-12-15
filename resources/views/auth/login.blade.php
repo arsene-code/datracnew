43e81b835f9971c807fadf0e608e4c407d92d095
NJOlAI <37351546+PrimeClet@users.noreply.github.com>
2021-12-16 00:42:05 +0100

    Design FrontEnd

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Datrac-Login</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="assets/dashboard/dashboard/img/favicon.png" rel="icon">
+    <link href="assets/dashboard/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.gstatic.com" rel="preconnect">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/dashboard/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/dashboard/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/dashboard/vendor/remixicon/remixicon.css" rel="stylesheet">
+    <link href="assets/dashboard/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="assets/dashboard/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: NiceAdmin - v2.1.0
+    * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+
+<body>
+
+<main>
+    <div class="container">
+
+        <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+                        <div class="d-flex justify-content-center py-2">
+                            <a href="/" class="logo d-flex align-items-center w-auto">
+{{--                                <img src="assets/dashboard/img/logo.png" alt="">--}}
+                                <span class="d-block d-lg-block">Datrac</span>
+                            </a>
+                        </div><!-- End Logo -->
+
+                        <div class="card mb-4">
+
+                            <div class="card-body">
+
+                                <div class="pt-4 pb-2 mb-4">
+                                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                                    <p class="text-center small">Enter your username or email & password to login</p>
+                                </div>
+
+                                <form class="row g-3 needs-validation" method="POST" action="{{ route('login') }}">
+                                    @csrf
+                                    <div class="col-12">
+                                        <label for="login" class="col-md-12 mb-1 col-form-label text-md-right">Username or Email</label>
+                                        <input id="login" type="text" class="form-control {{ $errors->has('email') || $errors->has('username') ? 'is-invalid' : ''}} " name="login" value="{{ old('login') }}" required autocomplete="login" autofocus>
+
+                                        @if ($errors->has('email') || $errors->has('username'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('email') ?: $errors->first('username') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+
+                                    <div class="col-12">
+                                        <label for="yourPassword" class="form-label">Password</label>
+                                        <input type="password" name="password" class="form-control" @error('password') is-invalid @enderror" id="yourPassword" required>
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="col-12">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe" {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="rememberMe">Remember me</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <button class="btn btn-primary w-100" type="submit">Login</button>
+                                    </div>
+                                    <div class="col-12">
+                                        <p class="small mb-0">Don't have account? <a href="/register">Create an account</a></p>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </div>
+</main><!-- End #main -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="assets/dashboard/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="assets/dashboard/vendor/php-email-form/validate.js"></script>
+<script src="assets/dashboard/vendor/quill/quill.min.js"></script>
+<script src="assets/dashboard/vendor/tinymce/tinymce.min.js"></script>
+<script src="assets/dashboard/vendor/simple-datatables/simple-datatables.js"></script>
+<script src="assets/dashboard/vendor/chart.js/chart.min.js"></script>
+<script src="assets/dashboard/vendor/apexcharts/apexcharts.min.js"></script>
+<script src="assets/dashboard/vendor/echarts/echarts.min.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/dashboard/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>eStartup Bootstrap Template - Index</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="/assets/frontend/img/favicon.png" rel="icon">
+    <link href="/assets/frontend/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Roboto:100,300,400,500,700|Philosopher:400,400i,700,700i" rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="/assets/frontend/vendor/aos/aos.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="/assets/frontend/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="/assets/frontend/css/style.css" rel="stylesheet">
+
+    <!-- =======================================================
+    * Template Name: eStartup - v4.7.0
+    * Template URL: https://bootstrapmade.com/estartup-bootstrap-landing-page-template/
+    * Author: BootstrapMade.com
+    * License: https://bootstrapmade.com/license/
+    ======================================================== -->
+</head>
+
+<body>
+
+<!-- ======= Header ======= -->
+<header id="header" class="header fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+        <div id="logo">
+            <h1><a href="/"><span>e</span>Datrac</a></h1>
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html"><img src="/assets/frontend/img/logo.png" alt="" title="" /></a>-->
+        </div>
+
+        <nav id="navbar" class="navbar">
+            <ul>
+                <li><a class="nav-link scrollto" href="#hero">Home</a></li>
+                <li><a class="nav-link scrollto active" href="/login">Login</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav><!-- .navbar -->
+
+    </div>
+</header><!-- End Header -->
+
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+    <div class="hero-container" data-aos="fade-in">
+        <h1>Bienvenue Sur Datrac</h1>
+        <h2>Votre Plateforme De Dématérialisation du Processus de Soin</h2>
+        <img src="/assets/frontend/img/homeCare.svg" alt="Hero Imgs" data-aos="zoom-out" data-aos-delay="100">
+        <a href="/login" class="btn-get-started scrollto">Commencez !</a>
+        <div class="btns">
+            <a href="#"><i class="fa fa-apple fa-3x"></i> App Store</a>
+            <a href="#"><i class="fa fa-play fa-3x"></i> Google Play</a>
+            <a href="#"><i class="fa fa-windows fa-3x"></i> windows</a>
+        </div>
+    </div>
+</section><!-- End Hero Section -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="/assets/frontend/vendor/aos/aos.js"></script>
+<script src="/assets/frontend/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/frontend/vendor/glightbox/js/glightbox.min.js"></script>
+<script src="/assets/frontend/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="/assets/frontend/vendor/php-email-form/validate.js"></script>
+
+<!-- Template Main JS File -->
+<script src="/assets/frontend/js/main.js"></script>
+
+</body>
+
+</html>
